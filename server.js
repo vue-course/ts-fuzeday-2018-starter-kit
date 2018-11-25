d052366b92d5fc533535556f6e5ca9303a2a34f5
David Meir-Levy <davidmeirlevy@gmail.com>
2018-11-26 00:14:06 +0200

    ping

--- server.js
+++ server.js
@@ -2,16 +2,23 @@ const cors = require('cors');
 const morgan = require('morgan');
 const express = require('express');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 app.use(cors());
 app.use(morgan('combined'));
 
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/dist/index.html');
+});
+
+
+app.get('/api/ping', function (req, res) {
+	res.status(200).end();
 });
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 3000, () => {
-	console.log('app started on port: ', process.env.PORT || 3000);
+app.listen(port, () => {
+	console.log('app started on port: ', port);
 });

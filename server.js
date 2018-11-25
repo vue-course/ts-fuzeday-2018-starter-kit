be54ae92c69bb041ac30f8bc812a71f055ea5db1
David Meir-Levy <davidmeirlevy@gmail.com>
2018-11-26 00:04:48 +0200

    expose index.html file

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const express = require('express');
 const app = express();
 app.use(cors());
 app.use(morgan('combined'));
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 3000, () => {

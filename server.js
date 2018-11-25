f94f4fa871ab699942c4064ad1318a1c81d48cee
David Meir-Levy <davidmeirlevy@gmail.com>
2018-11-25 23:58:18 +0200

    minor change for static path

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 app.use(cors());
 app.use(morgan('combined'));
-app.use(express.static('dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log('app started on port: ', process.env.PORT || 3000);

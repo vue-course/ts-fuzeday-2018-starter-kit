418b4a0f749a41c16f51e8b6d2d219adfdb9ef87
David Meir-Levy <davidmeirlevy@gmail.com>
2018-11-25 23:48:09 +0200

    add cors and morgan

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+const cors = require('cors');
+const morgan = require('morgan');
 const express = require('express');
 
 const app = express();
+app.use(cors());
+app.use(morgan('combined'));
 app.use(express.static('dist'));
-app.listen(process.env.PORT || 3000);
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log('app started on port: ', process.env.PORT || 3000);
+});

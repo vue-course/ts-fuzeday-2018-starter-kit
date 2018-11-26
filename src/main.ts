8f27f20ffcc9780b641949dd7d8731f67a73d795
shaul naim <shaul.naim@forescout.com>
2018-11-26 17:55:12 +0200

    beutify list of products using elements-ui cards

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import router from './router'
 import store from './store'
 import './styles/main.scss';
 import './plugins/element.js'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
 
+Vue.use(ElementUI);
 
 new Vue({
     router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,20 @@
 <template>
 	<div class="home">
 		<HelloWorld :msg="checkout"/>
-
-		<div v-for="product in products">
-			<h3>{{product.title}}</h3>
-			<p>price: {{product.variants[0].price}}</p>
-		</div>
+            <el-row :gutter="20">
+                <el-col :span="6" v-for="(product, index) in products" :key="`product-${index}`" :offset='2'>
+                    <el-card :body-style="{ padding: '0px' }">
+                    <img :src="product.images[0].src" class="image">
+                    <div style="padding: 14px;">
+                        <span>{{product.title}}</span>
+                        <div class="bottom">
+                        <time class="price">price: {{product.variants[0].price}}</time>
+                        <el-button type="success" round class="button">Buy</el-button>
+                        </div>
+                    </div>
+                    </el-card>
+                </el-col>
+            </el-row>
 	</div>
 </template>
 
